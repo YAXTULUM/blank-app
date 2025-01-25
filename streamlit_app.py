2f6ac46123b59075d110d7fe2707c25936312011
YAXTULUM <81526684+YAXTULUM@users.noreply.github.com>
2025-01-24 16:55:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ st.markdown(
 
         /* Dropdown Button Styling */
         .dropdown-button {
-            background-color: #007BFF;
+            background-color: #94b1cf00;
             color: white;
             font-size: 12px;
             font-weight: bold;
