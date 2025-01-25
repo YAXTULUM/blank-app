7c4f6b4c2efd63fd3bb4c2758b08616e7c491592
YAXTULUM <81526684+YAXTULUM@users.noreply.github.com>
2025-01-24 17:05:56 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,20 @@ st.markdown(
             <a href="/?page=Contact">Contact</a>
         </div>
     </div>
+
+
+<!-- Title Section -->
+    <div class="title-section">
+        <h1>VillaTerras Ai | Real Estate Dashboard</h1>
+        <h2>Real Estate Dashboard</h2>
+        <p>Welcome to the <strong>VillaTerras Ai Real Estate Dashboard</strong>.</p>
+        <p>All Real Estate Knowledge in One Place.</p>
+        <p>Analyze, compare, and manage properties with advanced metrics and tools.</p>
+    </div>
+
+
+
+    
     """,
     unsafe_allow_html=True
 )
